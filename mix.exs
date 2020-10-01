078921c3bc6f74e7d30842738e0dd8e390787073
Alexandre de Souza <alexandre@aledsz.com.br>
2020-10-01 01:42:32 -0300

    Update mix file and add dependencies

--- mix.exs
+++ mix.exs
@@ -1,28 +1,41 @@
 defmodule EctoPolymorphic.MixProject do
   use Mix.Project
 
-  def project do
-    [
+  def project,
+    do: [
       app: :ecto_polymorphic,
       version: "0.1.0",
       elixir: "~> 1.8",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      description: "Library to polymorphism association for Ecto",
+      package: [
+        links: %{
+          github: "https://github.com/aleDsz/ecto-polymorphic"
+        },
+        licenses: ["MIT"]
+      ],
+      docs: docs(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
-  end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
-    [
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  def application,
+    do: [
       extra_applications: [:logger]
     ]
-  end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+  defp deps,
+    do: [
+      {:ecto, "~> 3.4"},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
-  end
+
+  defp docs,
+    do: [
+      main: "EctoPolymorphic",
+      extras: ["README.md"]
+    ]
 end
