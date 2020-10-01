e3cada063513da60e141293e1a236a4dadb744ad
Alexandre de Souza <alexandre@aledsz.com.br>
2020-10-01 01:44:03 -0300

    Update test helper

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,12 @@
+# For tasks/generators testing
+Mix.start()
+Mix.shell(Mix.Shell.Process)
+System.put_env("ECTO_EDITOR", "")
+Logger.configure(level: :info)
+
+Code.require_file("support/repo.exs", __DIR__)
 ExUnit.start()
+
+if function_exported?(ExUnit, :after_suite, 1) do
+  ExUnit.after_suite(fn _ -> Mix.shell(Mix.Shell.IO) end)
+end
